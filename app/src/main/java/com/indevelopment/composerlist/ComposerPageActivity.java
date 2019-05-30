7d8efed817a1178d14170e2c3aeb6185cebd802d
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 17:08:25 +0700

    Add ComposerPageActivity class

---
+++ app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
@@ -0,0 +1,14 @@
+package com.indevelopment.composerlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComposerPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_composer_page);
+    }
+}
