12c74ed78fd08c8214f18e45bc8f2f172c85f73d
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 22:15:01 +0700

    Added appropriate TextView fields.

--- app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
+++ app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
@@ -3,6 +3,7 @@ package com.indevelopment.composerlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.indevelopment.composerlist.Model.Composer;
 
@@ -11,6 +12,8 @@ public class ComposerPageActivity extends AppCompatActivity {
     public static String COMPOSER = "COMPOSER";
 
     private Composer mComposer;
+
+    TextView mComposerName, mComposerDetail, mComposerBorn, mComposerDied;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class ComposerPageActivity extends AppCompatActivity {
 
         if(bundle != null) {
             mComposer = bundle.getParcelable(COMPOSER);
+
 
         }
     }
