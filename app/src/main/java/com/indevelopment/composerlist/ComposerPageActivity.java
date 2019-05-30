4e8f7516c448b78a987a1b53afa1769528f35d15
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 17:12:29 +0700

    Add COMPOSER field to be intent extra key

--- app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
+++ app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class ComposerPageActivity extends AppCompatActivity {
 
+    public static String COMPOSER = "COMPOSER";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
