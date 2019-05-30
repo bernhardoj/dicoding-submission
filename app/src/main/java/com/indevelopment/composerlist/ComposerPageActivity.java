67d4e32f6fb42935681393aefe1139068eee1d43
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 17:16:37 +0700

    Added some attributes including Bundle in onCreate method.

--- app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
+++ app/src/main/java/com/indevelopment/composerlist/ComposerPageActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.indevelopment.composerlist.Model.Composer;
+
 public class ComposerPageActivity extends AppCompatActivity {
 
     public static String COMPOSER = "COMPOSER";
+    private String mActivityLabel = "";
+
+    private Composer mComposer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composer_page);
+
+        Bundle bundle = getIntent().getExtras();
     }
 }
