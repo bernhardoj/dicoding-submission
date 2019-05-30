f114be5e421c4fe3365bf2b1c79d8d4e0f48cec9
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 13:34:11 +0700

    Added fields and constructor.

--- app/src/main/java/com/indevelopment/composerlist/Model/Composer.java
+++ app/src/main/java/com/indevelopment/composerlist/Model/Composer.java
@@ -1,4 +1,62 @@
 package com.indevelopment.composerlist.Model;
 
 public class Composer {
+    private String name, detail, born, died, photo, signature;
+
+    public Composer(String name, String detail, String born, String died, String photo, String signature) {
+        this.name = name;
+        this.detail = detail;
+        this.born = born;
+        this.died = died;
+        this.photo = photo;
+        this.signature = signature;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getBorn() {
+        return born;
+    }
+
+    public void setBorn(String born) {
+        this.born = born;
+    }
+
+    public String getDied() {
+        return died;
+    }
+
+    public void setDied(String died) {
+        this.died = died;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
 }
