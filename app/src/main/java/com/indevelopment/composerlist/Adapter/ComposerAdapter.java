5608bcb50db328988393ccd8d30d38a3f0a9235f
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 15:00:45 +0700

    Added inner class to load image from an URL.

--- app/src/main/java/com/indevelopment/composerlist/Adapter/ComposerAdapter.java
+++ app/src/main/java/com/indevelopment/composerlist/Adapter/ComposerAdapter.java
@@ -1,5 +1,9 @@
 package com.indevelopment.composerlist.Adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.indevelopment.composerlist.Model.Composer;
 import com.indevelopment.composerlist.R;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class ComposerAdapter extends RecyclerView.Adapter<ComposerAdapter.ViewHolder> {
@@ -52,6 +57,31 @@ public class ComposerAdapter extends RecyclerView.Adapter<ComposerAdapter.ViewHo
 
         void bind(Composer composer) {
             
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
         }
     }
 }
