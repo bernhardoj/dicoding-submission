396b0f0876bd40315d75a4af88bf348b4d063b90
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-05-30 14:24:44 +0700

    Generated the adapter on ComposerAdapter.

--- app/src/main/java/com/indevelopment/composerlist/Adapter/ComposerAdapter.java
+++ app/src/main/java/com/indevelopment/composerlist/Adapter/ComposerAdapter.java
@@ -1,4 +1,51 @@
 package com.indevelopment.composerlist.Adapter;
 
-public class ComposerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.indevelopment.composerlist.Model.Composer;
+import com.indevelopment.composerlist.R;
+
+import java.util.ArrayList;
+
+public class ComposerAdapter extends RecyclerView.Adapter<ComposerAdapter.ViewHolder> {
+
+    private ArrayList<Composer> mComposers;
+
+    public ComposerAdapter(ArrayList<Composer> composers) {
+        mComposers = composers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.composer_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(mComposers.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mComposers.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        void bind(Composer composer) {
+
+        }
+    }
 }
